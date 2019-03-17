324bab75058b8513942e0ce4bfff8d2fd43a36fe
Rahul Bharati <rahulrbharati@gmail.com>
2019-03-17 10:03:05 +0530

    custom tags and filter

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -1,4 +1,11 @@
 from django import template
+from django.db.models import Count
+from django.utils.safestring import mark_safe
+
+
+import markdown
+
+
 from ..models import Post
 
 register = template.Library()
@@ -8,7 +15,17 @@ def total_posts():
     return Post.published.count()
 
 @register.inclusion_tag('blog/post/latest_posts.html')
-
 def show_latest_posts(count=5):
     latest_posts = Post.published.order_by('-publish')[:count]
     return {'latest_posts': latest_posts}
+
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    return Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
+
+# following lines create filter
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
