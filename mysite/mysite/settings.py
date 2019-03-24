9f082893e0b7d4fca3b943671de9350bdc2c2a8a
Rahul Bharati <rahul@rahul-pc.localdomain>
2019-03-24 10:48:03 +0530

    settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -78,9 +78,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'blog',
+        'PASSWORD': '72485',
     }
 }
 
