d0eadf52eac2abff7b630e79981c79dde121d5c5
Rahul Bharati <rahul@rahul-pc.localdomain>
2019-03-24 11:38:27 +0530

    blog application created

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,5 @@
+# from django.contrib.postgres.search import SearchVector, SearchQuery,SearchRank
+from django.contrib.postgres.search import TrigramSimilarity
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView   #IMPORTING Listview
@@ -8,7 +10,7 @@ from django.db.models import Count
 from taggit.models import Tag
 
 
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from .models import Post, Comment
 
 # Create your views here.
@@ -92,3 +94,20 @@ def post_share(request, post_id):
     else:
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {'post': post,'form': form, 'sent':sent})
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            # search_vector = SearchVector('title', weight='A') + SearchVector('body',weight='B')
+            # search_query = SearchQuery(query)
+            # results = Post.objects.annotate(search=search_vector,rank=SearchRank(search_vector, search_query)).filter(rank__gte=0.3).order_by('-rank')
+            results = Post.objects.annotate(similarity=TrigramSimilarity('title', query),).filter(similarity__gt=0.3).order_by('-similarity')
+
+    return render(request,'blog/post/search.html',{'form': form,
+                                                    'query': query,
+                                                    'results': results})
